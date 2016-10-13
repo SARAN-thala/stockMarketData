d85ef9c0b6f48020692dcad480d0aff009246f0c
Saran <saranrajmalar@gmail.com>
2016-10-13 23:51:51 +0530

    Saran #introduce line graph

--- public/js/visualize.js
+++ public/js/visualize.js
@@ -1,21 +1,23 @@
+const DATA = '../data/Price-volume-data-TCSEQN.csv';
+
 const parseQuote = function (quote) {
     quote['Date'] = new Date(quote['Date']);
     quote['Close Price'] = +quote['Close Price'];
     return quote;
 };
 
-const WIDTH = 1000;
+const WIDTH = 1420;
 const HEIGHT = 650;
 const MARGIN = 50;
 
-var loadChart = function (quotes) {
+const loadChart = function (quotes) {
 
-    var svg = d3.select('.container').append('svg')
+    let svg = d3.select('.container').append('svg')
         .attr('width', WIDTH)
         .attr('height', HEIGHT);
 
-    var dateRange = d3.extent(quotes, quote => (quote['Date']));
-    var priceRange = d3.extent(quotes, quote => (quote['Close Price']));
+    let dateRange = d3.extent(quotes, quote => (quote['Date']));
+    let priceRange = d3.extent(quotes, quote => (quote['Close Price']));
 
     priceRange[0] -= 5;
     priceRange[1] += 5;
@@ -24,18 +26,18 @@ var loadChart = function (quotes) {
     dateRange[1].setDate(dateRange[1].getDate() + 2);
 
 
-    var xScale = d3.scaleTime()
+    let xScale = d3.scaleTime()
         .domain(dateRange)
         .range([0, WIDTH - (2 * MARGIN)]);
 
-    var yScale = d3.scaleLinear()
+    let yScale = d3.scaleLinear()
         .domain(priceRange)
         .range([HEIGHT - (2 * MARGIN), 0]);
 
-    var xAxis = d3.axisBottom(xScale).ticks(5);
-    var yAxis = d3.axisLeft(yScale).ticks(10);
+    let xAxis = d3.axisBottom(xScale);
+    let yAxis = d3.axisLeft(yScale).ticks(10);
 
-    var g = svg.append('g')
+    let g = svg.append('g')
         .attr('transform', `translate(${MARGIN}, ${MARGIN})`);
 
     svg.append('g')
@@ -46,13 +48,23 @@ var loadChart = function (quotes) {
         .attr('transform', `translate(${MARGIN - 10}, ${MARGIN})`)
         .call(yAxis);
 
+    //--line--
+    let path = g.append('path');
+
+    let line = d3.line()
+        .x(q => (xScale(q['Date'])))
+        .y(q => (yScale(q['Close Price'])));
+
+    path.attr('d',line(quotes));
+
+    //--circle---
     g.selectAll('circle').data(quotes)
         .enter().append('circle')
         .attr('r', 4)
         .append('title').text(q => (`Date: ${q['Date'].toISOString().split('T')[0]},
 Price: ${q['Close Price']}`));
 
-    var circles = g.selectAll('circle');
+    let circles = g.selectAll('circle');
 
     circles.attr('cx', q => (xScale(q['Date'])))
         .attr('cy', q => (yScale(q['Close Price'])));
@@ -60,4 +72,4 @@ Price: ${q['Close Price']}`));
     g.selectAll('circle').exit().remove();
 };
 
-d3.csv('../data/Price-volume-data-TCSEQN.csv', parseQuote, loadChart);
+d3.csv(DATA, parseQuote, loadChart);
